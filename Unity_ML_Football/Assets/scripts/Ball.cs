13bd7f4984444710013afed721244e2639940de7
jessica880512 <55180625+jessica880512@users.noreply.github.com>
2020-04-28 08:20:29 +0800

    尚未輸出

---
+++ Unity_ML_Football/Assets/scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //足球是否進球門
+    public static bool complete;
+
+
+    //觸發開始事件:碰到勾選is trigger物件會執行一次
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "進球感應區")
+        {
+            complete = true;
+        }
+    }
+
+}
